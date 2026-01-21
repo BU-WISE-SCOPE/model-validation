7466e2d74a5c96579b04e4bd359e1a3ec72e3349
dokyun-kim4 <donny5557@gmail.com>
2026-01-21 15:41:49 -0500

    Notebook for testing detection of individual sound

--- azure_recognizer.py
+++ azure_recognizer.py
@@ -4,7 +4,7 @@ import azure.cognitiveservices.speech as sdk
 def recognize_from_wav(wav_file: str, ref_text: str) -> sdk.SpeechRecognitionResult:
     # Create Speech and Push stream configs
     with open("azure_key.txt") as f:
-        key = f.read()
+        key = f.read().strip("\n")
 
     speech_config = sdk.SpeechConfig(subscription=key, region="eastus")
     speech_config.speech_recognition_language = "en-US"
@@ -21,10 +21,10 @@ def recognize_from_wav(wav_file: str, ref_text: str) -> sdk.SpeechRecognitionRes
         granularity=sdk.PronunciationAssessmentGranularity.Phoneme,
         enable_miscue=True,
     )
-    # pron_config.phoneme_alphabet = "IPA"
+    pron_config.phoneme_alphabet = "IPA"
+    pron_config.nbest_phoneme_count = 5
     pron_config.enable_prosody_assessment()
     pron_config.apply_to(recognizer)
-
     result = recognizer.recognize_once()
     return result
 
@@ -34,17 +34,21 @@ def display_result(
 ) -> sdk.PronunciationAssessmentResult:
     pa_result = sdk.PronunciationAssessmentResult(result)
 
-    print("\n=== PRONUNCIATION ASSESSMENT ===")
-    print("Accuracy Score:      ", pa_result.accuracy_score)
-    print("Completeness Score:  ", pa_result.completeness_score)
-    print("Fluency Score:       ", pa_result.fluency_score)
-    print("Pronunciation Score: ", pa_result.pronunciation_score)
-    print("Prosody Score:       ", pa_result.prosody_score)
+    # print("\n=== PRONUNCIATION ASSESSMENT ===")
+    # print("Accuracy Score:      ", pa_result.accuracy_score)
+    # print("Completeness Score:  ", pa_result.completeness_score)
+    # print("Fluency Score:       ", pa_result.fluency_score)
+    # print("Pronunciation Score: ", pa_result.pronunciation_score)
+    # print("Prosody Score:       ", pa_result.prosody_score)
 
-    print("\n=== WORD-LEVEL DETAILS ===")
+    # print("\n=== WORD-LEVEL DETAILS ===")
     for word in pa_result.words:
         print(f"{word.word}: accuracy={word.accuracy_score}, error={word.error_type}")
 
+
         for phoneme in word.phonemes:
-            print(f"  {phoneme.phoneme}: accuracy={phoneme.accuracy_score}")
+            print("------------------")
+            for nbest_phoneme in phoneme.nbest_phonemes:
+                print(f"  {nbest_phoneme.phoneme}: accuracy={nbest_phoneme.score}")    
+            # print(f"  {phoneme.phoneme}: accuracy={phoneme.accuracy_score}")
     return pa_result
